dba984e7ea6ac887184ecaf1ddc0366f51390a09
kapilansh-10 <kapilpatil2004@gmail.com>
2025-02-09 20:46:02 +0530

    Add user authentication with signup and login functionality

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express =  require("express");
 const path = require('path')
-const urlRoute = require('./routes/url')
-const staticRoute = require('./routes/staticRouter')
+const cookieParser = require('cookie-parser')
 const URL = require("./models/url")
 const { connectToMongoDB } = require("./connect")
+const {restrictToLoggedinUserOnly, checkAuth} = require('./middleware/auth')
+
 const app = express();
 const PORT = 8001;
+
+const urlRoute = require('./routes/url')
+const staticRoute = require('./routes/staticRouter')
+const userRoute = require('./routes/user')
 
 connectToMongoDB('mongodb://127.0.0.1:27017/short-url')
 .then(() => console.log("MongoDB connected"))
@@ -15,14 +20,17 @@ app.set('views', path.resolve("./views"))
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
 
 app.get('/test', async (req,res) => {
     const allUrls = await URL.find({})
     return res.render('home')
 })
 
-app.use("/url", urlRoute)
-app.use('/', staticRoute)
+app.use(checkAuth)
+app.use("/url", restrictToLoggedinUserOnly, urlRoute)
+app.use('/', staticRoute, checkAuth)
+app.use('/user', userRoute)
 
 app.get("/url/:shortId", async (req,res) => {
     const shortId = req.params.shortId
--- models/url.js
+++ models/url.js
@@ -13,6 +13,10 @@ const urlSchema = new mongoose.Schema({
     visitHistory: [{
         timestamps: { type: Number }
     }],
+    createdBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'users'
+    }
 },{ timestamps: true }
 )
 
